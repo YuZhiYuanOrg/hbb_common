e1c3c02f5a03f5e1829819a37638457027f6e591
YuZhiYuanDev <203504060+YuZhiYuanDev@users.noreply.github.com>
2025-11-08 20:57:16 +0800

    refactor: replace hardcoded credentials with compile-time environment variables (#1)
    * update RENDEZVOUS_SERVERS and RS_PUB_KEY to use environment variables with fallback values
    
    * update version_check_request to use UPDATE_SERVER environment variable for URL

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,14 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[match option_env!("RENDEZVOUS_SERVERS") {
+    Some(servers) => servers,
+    None => "rs-ny.rustdesk.com",
+}];
+pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
+    Some(key) => key,
+    None => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+};
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
